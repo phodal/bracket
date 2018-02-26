e3d39b2a39ff1f95322c63b7aadf3448d7f71f07
Phodal HUANG <h@phodal.com>
2018-02-26 14:29:38 +0800

    [T] config API

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # bracket
+
 A Serverless IoT Server for Fun
+
+**Features**
+
+ - QRCode
+ - On/Off Toggle
+ - JSON support
+ - Wechat Login
+
+Setup
+---
+
+```
+npm install -g serverless
+```
+
+1.install
+
+```
+yarn install
+```
+
+2.create domain
+
+```
+sls create_domain
+```
+
+3.deloy
+
+```
+sls deploy
+```
+
+## LICENSE
+
+[![Phodal's Idea](http://brand.phodal.com/shields/idea-small.svg)](http://ideas.phodal.com/)
+
+© 2018 A [Phodal Huang](https://www.phodal.com)'s [Idea](http://github.com/phodal/ideas).  This code is distributed under the MIT license. See `LICENSE` in this directory.
+
--- qrcode/index.js
+++ qrcode/index.js
@@ -1,15 +1,43 @@
 module.exports.on = (event, context, callback) => {
-
-}
+  console.log('on');
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      action: 'on',
+      input: context,
+    })
+  });
+};
 
 module.exports.off = (event, context, callback) => {
-
-}
+  console.log('off');
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      action: 'off',
+      input: context,
+    })
+  });
+};
 
 module.exports.main = (event, context, callback) => {
-
-}
+  console.log('main');
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      action: 'main',
+      input: context,
+    })
+  });
+};
 
 module.exports.status = (event, context, callback) => {
-
-}
+  console.log('status');
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      action: 'status',
+      input: context,
+    })
+  });
+};
