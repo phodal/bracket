1f9971dadb1ad632afae9f4e51dfeb8c6c96ebe7
Phodal HUANG <h@phodal.com>
2018-02-26 14:10:04 +0800

    [T] init boilerplate

---
+++ qrcode/index.js
@@ -0,0 +1,15 @@
+module.exports.on = (event, context, callback) => {
+
+}
+
+module.exports.off = (event, context, callback) => {
+
+}
+
+module.exports.main = (event, context, callback) => {
+
+}
+
+module.exports.status = (event, context, callback) => {
+
+}
