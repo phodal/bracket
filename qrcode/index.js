c013ff0534ba1089f064e623f6f8de5d0cf3cdc3
Phodal HUANG <h@phodal.com>
2018-02-26 16:01:25 +0800

    [T] try to update status

--- qrcode/index.js
+++ qrcode/index.js
@@ -1,24 +1,7 @@
-module.exports.on = (event, context, callback) => {
-  console.log('on');
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      action: 'on',
-      input: context,
-    })
-  });
-};
+const AWS = require('aws-sdk');
 
-module.exports.off = (event, context, callback) => {
-  console.log('off');
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      action: 'off',
-      input: context,
-    })
-  });
-};
+const tableName = process.env.DYNAMODB_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.main = (event, context, callback) => {
   console.log('main');
@@ -28,6 +11,80 @@ module.exports.main = (event, context, callback) => {
       action: 'main',
       input: context,
     })
+  });
+};
+
+module.exports.on = (event, context, callback) => {
+  const uuid = event.pathParameters.uuid;
+  let params = {
+    TableName: tableName,
+    Key: {
+      "uuid": uuid
+    },
+    UpdateExpression: "set status=:s",
+    ExpressionAttributeValues:{
+      ":s": "1"
+    },
+    ReturnValues:"UPDATED_NEW"
+  };
+
+  docClient.update(params, function (err, data) {
+    if (err) {
+      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+
+      callback(null, {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: "Unable to update item. Error JSON:" + JSON.stringify(err, null, 2)
+        })
+      });
+    } else {
+      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          results: "UpdateItem succeeded:" + JSON.stringify(data, null, 2),
+        })
+      });
+    }
+  });
+};
+
+module.exports.off = (event, context, callback) => {
+  const uuid = event.pathParameters.uuid;
+  let params = {
+    TableName: tableName,
+    Key: {
+      "uuid": uuid
+    },
+    UpdateExpression: "set status=:s",
+    ExpressionAttributeValues:{
+      ":s": "0"
+    },
+    ReturnValues:"UPDATED_NEW"
+  };
+
+  docClient.update(params, function (err, data) {
+    if (err) {
+      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+
+      callback(null, {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: "Unable to update item. Error JSON:" + JSON.stringify(err, null, 2)
+        })
+      });
+    } else {
+      console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          results: "UpdateItem succeeded:" + JSON.stringify(data, null, 2),
+        })
+      });
+    }
   });
 };
 
